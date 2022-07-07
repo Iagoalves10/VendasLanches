983820be742e68ad8978daa5ae456e62c18926ae
Iago Alves <iago.alves@bit.com.br>
2022-07-07 14:02:49 -0300

    Primeiro Commit

---
+++ VendasLanche/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendasLanche.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
